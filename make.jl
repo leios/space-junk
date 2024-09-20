9a9be7985242eb2d9aa9d3b346e03efb54efe6f1
James Schloss <jrs.schloss@gmail.com>
2024-09-20 14:07:09 +0200

    messing around with rei chapters

--- make.jl
+++ make.jl
@@ -7,8 +7,9 @@ makedocs(
         "General Information" => "index.md",
         "Chapter 1: She's Just Like You" => "content/joanne_1.md",
         "Chapter 2: Where Else Would You Be?" => "content/jeb_1.md",
-        "Chapter 3: The Man the World Forgot" => "content/rei_1.md",
-        "Chapter 4: The Screens Are Gone" => "content/lars_1.md",
+        "Chapter 3: The Screens Are Gone" => "content/lars_1.md",
+        "Chapter 4: A Story I Know You Know" => "content/lars_2.md",
+        "Chapter 5: The Man the World Forgot" => "content/rei_2.md",
     ],
 )
 
